f34ab44380bd63c7a0f182bb957b68de7167f943
Sean Klechak <sklechak@students.mchenry.edu>
2018-09-09 14:36:02 -0500

    Initial commit

---
+++ src/gunshelf.java
@@ -0,0 +1,11 @@
+public class gunshelf {
+
+    public static void main(String[] args){
+        String[] gun_store = {"Springfield","Glock","Sig Sauer","Ruger","Smith & Wesson","Mossberg"};
+
+      for(int i = 0; i < gun_store.length; i = i + 1) {
+            System.out.println(gun_store[i]);
+        }
+
+    }
+}
